04057a25b0ff33eb2a85c06521d2f6e6a0aa97b9
Spexzee_Abu <104344646+spexzee@users.noreply.github.com>
2024-02-29 00:27:21 +0530

    Update Nav.jsx

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -28,7 +28,7 @@ const Nav = () => {
           height={30}
           className='object-contain'
         />
-        <p className='logo_text'>Promptopia</p>
+        <p className='logo_text'>Prompt-World</p>
       </Link>
 
       {/* Desktop Navigation */}
